536d0b912ad07bf9750b0f7510035693457d6e8f
t-nan <t-nan@yandex.ru>
2020-09-24 03:15:30 +0300

    add show,create methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,8 +6,24 @@ class ArticlesController < ApplicationController
   def new
   end
 
-  def create
+  def show
+    @article=Article.find(params[:id])
+  end
 
+  def create
+  @article=Article.new (article_params)
+
+    if @article.save
+       redirect_to @article
+    else
+      render action: 'new'
+    end
+  end
+
+   private
+
+  def article_params
+    params.require(:article).permit(:title,:text)
   end
   
 end
