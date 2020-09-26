35f90d9ac91072d81973f137059bd58631e7bf11
t-nan <t-nan@yandex.ru>
2020-09-26 23:01:59 +0300

    add model Comment,references,comments table

---
+++ app/models/comment.rb
@@ -0,0 +1,8 @@
+class Comment < ApplicationRecord
+
+  belongs_to :article
+
+  validates :author, presence: true ,length: {minimum: 2}
+  validates :body, presence: true ,length: {minimum: 5}
+  
+end
