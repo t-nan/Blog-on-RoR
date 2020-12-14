3580f5ae38048dca0fdd18405ace9949663707a7
t-nan <t-nan@yandex.ru>
2020-12-14 21:02:46 +0300

    add subject method for Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,8 @@ class Article < ApplicationRecord
   validates :title, presence: true ,length: {minimum: 2}
   validates :text, presence: true ,length: {minimum: 5}
 
+  def subject
+    title
+  end
+
 end
